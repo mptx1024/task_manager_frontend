511639e5c9b8dcc2cf68c344c663619ba8f47128
Frank Jia <frankjzy@gmail.com>
2023-02-09 18:43:16 -0600

    refactored for deployment

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3500/api/v1',
+    // baseUrl: 'http://localhost:3500/api/v1',
+    baseUrl: process.env.REACT_APP_API_END_POINT,
 
     prepareHeaders: (Headers, { getState }) => {
         const user = getState().auth.user; // Destruct user in state to get the token
